ad98476785f61571771c892c72b18fc515aac583
Remco Ridder <remco.ridder@knab.nl>
2022-09-30 13:55:11 +0200

    first commit for simple api project

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using Xunit;
+
+namespace ApiTester;
+
+public class Program
+{
+    protected readonly HttpClient Client;
+    private readonly Uri Url;
+
+    public Program()
+    {
+        Client = new HttpClient();
+        var getUrl = Environment.GetEnvironmentVariable("url");
+        Url = new(getUrl);
+    }
+
+    [Fact]
+    public async void GetToken()
+    {
+        try
+        {
+            var client = new HttpClient();
+            var userName = Environment.GetEnvironmentVariable("username");
+            var password = Environment.GetEnvironmentVariable("password");
+
+            var encoded = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", userName, password)));
+
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encoded);
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await Client.PostAsync(Url, null);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var content = response.Content.ReadAsStringAsync();
+            var actual = JsonConvert.DeserializeObject<AuthenticationResponse>(content.Result);
+            Assert.NotEmpty(actual.Token);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    [Fact]
+    public async void Test()
+    {
+        return;
+    }
+
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BackEndTests
+
+This project is a quick set up to run one or a few api requests. Nothing more that that. For more elaborate projects see other repos. 
